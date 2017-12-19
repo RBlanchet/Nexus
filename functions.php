0c167be6718a1a414b48f79351f1447ec62c23a9
admin <admin@iMac3-28.local>
2017-12-19 15:10:13 +0100

    FIRST COMMIT

---
+++ functions.php
@@ -0,0 +1,14 @@
+<!--  Fichier de fonctions concernant NEXUS -->
+<?php
+function div($col, $size)
+{
+  echo "<div class='col-$col-$size'>";
+}
+function p($text)
+{
+  echo "<p>$text</p>";
+}
+function enddiv()
+{
+  echo "</div>";
+}
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require_once('functions.php');
+
+div('md', 5);
+p('Salut je mapelle romain');
+enddiv();
